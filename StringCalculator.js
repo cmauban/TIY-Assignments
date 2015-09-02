48cb8e63052a9e6c5563c10588e118afcc7f8622
Camille Mauban <camille.mauban@gmail.com>
2015-09-02 19:35:22 -0400

    Update StringCalculator.js

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,11 +1,43 @@
-console.assert(add(0, 0), /* FILL ME IN */);
-console.assert(add(0, 1), /* FILL ME IN */);
-console.assert(add(0, 2), /* FILL ME IN */);
+var test = require('mocha').it,
+  expect = require('chai').expect;
+
+test('fromEnglish: "zero" through "nine"', function(){
+  expect(fromEnglish).to.exist;
+  expect(fromEnglish("zero")).to.equal(0);
+  expect(fromEnglish("one")).to.equal(1);
+  // more tests here, please...
+}); // END test(fromEnglish)
 
 /**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} sum of A and B
+ * @param {String} word representing an English number
+ * @return {Number} value corresponding to `word`
  */
-function add(A, B){
-} // END add
+function fromEnglish(word){
+  // fill this in after writing tests...
+} // END fromEnglish
+
+test('plus: ("zero", "zero") through ("zero", "nine")', function(){
+  expect(plus).to.exist;
+  expect(plus("zero", "zero")).to.equal(0);
+  expect(plus("zero", "one")).to.equal(1); // change just one thing!
+  expect(plus("zero", "two")).to.equal(FILL_ME_IN); // ha!
+  // write more tests like those...
+}); // END test(plus)
+
+/**
+ * @param {String} A representing an English number word
+ * @param {String} B representing an English number word
+ * @return {Number} the value of `A` added to `B`
+ */
+function plus(A, B){
+  // write those tests first, please...
+} // END plus
+
+test('minus: ("zero", "zero") through ("zero", "nine")', function(){
+  expect(minus).to.exist;
+  expect(minus("zero", "zero")).to.equal(0);
+  expect(minus("zero", "one")).to.equal(-1);
+  // etc, etc, etc
+}); // END test(minus)
+
+// What about multiplication? Division? Exponents?
