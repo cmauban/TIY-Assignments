6efada1d5485ad9a5528e2f89efffb46b8ddc6b6
Camille Mauban <camille.mauban@gmail.com>
2015-09-02 20:00:31 -0400

    updated

--- BasicCalculator.js
+++ BasicCalculator.js
@@ -13,6 +13,64 @@ console.assert(add(0, 7), === 7);
 console.assert(add(0, 8), === 8);
 console.assert(add(0, 9), === 9);
 
+console.assert(add(1, 0), === 1);
+console.assert(add(1, 1), === 2);
+console.assert(add(1, 2), === 3);
+console.assert(add(1, 3), === 4);
+console.assert(add(1, 4), === 5);
+console.assert(add(1, 5), === 6);
+console.assert(add(1, 6), === 7);
+console.assert(add(1, 7), === 8);
+console.assert(add(1, 8), === 9);
+console.assert(add(1, 9), === 10);
+
+console.assert(add(2, 0), === 2);
+console.assert(add(2, 1), === 3);
+console.assert(add(2, 2), === 4);
+console.assert(add(2, 3), === 5);
+console.assert(add(2, 4), === 6);
+console.assert(add(2, 5), === 7);
+console.assert(add(2, 6), === 8);
+console.assert(add(2, 7), === 9);
+console.assert(add(2, 8), === 10);
+console.assert(add(2, 9), === 11);
+
+console.assert(add(3, 0), === 3);
+console.assert(add(3, 1), === 4);
+console.assert(add(3, 2), === 5);
+console.assert(add(3, 3), === 6);
+console.assert(add(3, 4), === 7);
+console.assert(add(3, 5), === 8);
+console.assert(add(3, 6), === 9);
+console.assert(add(3, 7), === 10);
+console.assert(add(3, 8), === 11);
+console.assert(add(3, 9), === 12);
+
+console.assert(add(4, 0), === 4);
+console.assert(add(4, 1), === 5);
+console.assert(add(4, 2), === 6);
+console.assert(add(4, 3), === 7);
+console.assert(add(4, 4), === 8);
+console.assert(add(4, 5), === 9);
+console.assert(add(4, 6), === 10);
+console.assert(add(4, 7), === 11);
+console.assert(add(4, 8), === 12);
+console.assert(add(4, 9), === 13);
+
+function add(A, B){
+  return A + B;
+};
+console.assert(add(1, 0), === 7);
+console.assert(add(2, 1), === 1);
+console.assert(add(3, 2), === 2);
+console.assert(add(4, 3), === 3);
+console.assert(add(5, 4), === 4);
+console.assert(add(6, 5), === 5);
+console.assert(add(7, 6), === 6);
+console.assert(add(8, 7), === 7);
+console.assert(add(9, 8), === 8);
+
+
 /**
  * @param {Number} A to add to B
  * @param {Number} B to add to A
@@ -37,6 +95,7 @@ console.assert(add(0, 9), === 9);
 };
  console.assert(times(0,0), === 0);
  console.assert(times(0,1), === 0);
+ console.assert(times(0,2), === 0);
  console.assert(times(0,3), === 0);
  console.assert(times(0,4), === 0);
  console.assert(times(0,5), === 0);
