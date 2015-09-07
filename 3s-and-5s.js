df2b8c27bcf415b3247e23030f38bebd1cdca39c
Camille Mauban <camille.mauban@gmail.com>
2015-09-06 22:42:11 -0400

    could not run the test on terminal, but passed the function on console.log

--- 3s-and-5s.js
+++ 3s-and-5s.js
@@ -3,16 +3,16 @@ var test = require('mocha').it,
     assert = require('chai').assert;  // OR use `assert.equal` etc
 
 test('three', function(){
-  // assert.equal(sumofthrees).to.exist;
-  // assert.equal(sumofthrees([3, 6, 9]), 18);
-  // assert.equal(sumofthrees([3, 6, 9, 12, 15, 18]), 63);
-  // assert.equal(sumofthrees([3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48]), 408);
-  expect(three(10)).to.equal(18);
-  expect(three(20)).to.equal(63);
+  // assert.equal(three).to.exist;
+  assert.equal(three([3, 6, 9]), 18);
+  assert.equal(three([3, 6, 9, 12, 15, 18]), 63);
+  assert.equal(sumofthrees([3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48]), 408);
+  // expect(three(10)).to.equal(18);
+  // expect(three(20)).to.equal(63);
   // expect(threes(50)).to.equal([3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48]);
 }); // END test 3s
 
-test('fives', function(){
+test('sumoffives', function(){
   assert.equal(sumoffives).to.exist;
   assert.equal(sumoffives([5]), 5);
   assert.equal(sumoffives([5, 10, 15]), 30);
@@ -22,23 +22,16 @@ test('fives', function(){
   // expect(fives(50)).to.equal([5, 10, 15, 20, 25, 30, 35, 40, 45])
 }); // END test 5s
 
-// function sumofthrees (A, B){
-//   var array1 = [3, 6, 9], array2 = [3, 6, 9, 12, 15, 18],
-//   array3 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48];
-//   array4 = [5], array5 = [5, 10, 15], array6= [5, 10, 15, 20, 25, 30, 35, 40, 45];
-// for(var i = 0; i < array1.length; i++);
-//   return sum;
-// }
+//could not get the tests to work, but i checked the function
+// in the console and it runs.
 
 var three = [];
 var five = [];
 var both = [];
-var All = three.concat(five, both);
-
 
 function Multiples (){
 
-  for(var i = 0; i < 1000; i++){
+  for(var i = 1; i < 1000; i++){
     if (i % 3 === 0 && i % 5 === 0){ //works if i divided by 3 and 5 has no remainder
       both.push(i); //var both = [array of both 3 and 5]
     } else if (i % 3 === 0){ //works if i divided by 3 has no remainders
@@ -47,7 +40,7 @@ function Multiples (){
       five.push(i); //var five = [array of multiples of 5]
     }
 }
-  for (var i = 0, sum = 0; i < All.length; sum += All[i++]) { //looping length of array
-  }
+  var All = three.concat(five, both); //joins all 3 arrays
 
+  for (var x = 0, sum = 0; x < All.length; sum += All[x++]);  //adds on to each iteration
 }
