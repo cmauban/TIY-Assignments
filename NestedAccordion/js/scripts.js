2aaffdd8cee2d32474e4964e52c567e902f5af54
Camille Mauban <camille.mauban@gmail.com>
2015-09-09 21:24:10 -0400

    scripts

--- NestedAccordion/js/scripts.js
+++ NestedAccordion/js/scripts.js
@@ -1,13 +1,23 @@
-//the interactive elements
-//blue words have the UI accordion drop down with more information.
-//the arrows are light grey until you hoover over them.
+// the interactive elements:
+// blue words have the UI accordion drop down with more information.
+// the arrows are light grey until you hoover over them.
+//
+// a class attribute in the list element for the headers is missing now.
+// I think this is what made all the headers of Nested Accordion interactive?
+//
+// also, in the HTML there is not a div id attribute that included style
+// anymore. the script tags.
+//
+// I didn't notice any HTML created in the JavaScript-less file.
+// the original file is index.js, vs. index.html
 
-establish a connection between JavaScript and HTML to make the
-website come alive. ex: linking the js file in the .html file.
-adding script tags.
-
-we need the js to connect the class by using getElementsByClassName
-to get the drop down information.
-
-we need an onclickeventhandler or onmouseover event for when the
-mouse moves over an element (arrow and header)
+// TO FIX:
+// establish a connection between JavaScript and HTML to make the
+// website come alive. ex: linking the js file in the .html file.
+// adding script tags.
+//
+// we need the js to connect the class by using getElementsByClassName
+// to get the drop down information. (need to add a class attribute to the list element)
+//
+// we need an onclickeventhandler or onmouseover event for when the
+// mouse moves over an element (arrow and header)
