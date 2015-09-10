779cf408980271d03c40c02eed86f1053f320684
Camille Mauban <camille.mauban@gmail.com>
2015-09-09 20:44:17 -0400

    idex and scripts

---
+++ NestedAccordion/js/scripts.js
@@ -0,0 +1,13 @@
+//the interactive elements
+//blue words have the UI accordion drop down with more information.
+//the arrows are light grey until you hoover over them.
+
+establish a connection between JavaScript and HTML to make the
+website come alive. ex: linking the js file in the .html file.
+adding script tags.
+
+we need the js to connect the class by using getElementsByClassName
+to get the drop down information.
+
+we need an onclickeventhandler or onmouseover event for when the
+mouse moves over an element (arrow and header)
