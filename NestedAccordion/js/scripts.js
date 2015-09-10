835a49601611caffe6b3858da414ce04bce195ca
Camille Mauban <camille.mauban@gmail.com>
2015-09-09 22:44:26 -0400

    scripts english

--- NestedAccordion/js/scripts.js
+++ NestedAccordion/js/scripts.js
@@ -5,8 +5,8 @@
 // a class attribute in the list element for the headers is missing now.
 // I think this is what made all the headers of Nested Accordion interactive?
 //
-// also, in the HTML there is not a div id attribute that included style
-// anymore. the script tags.
+// also, in the HTML there is not a div element with an id attribute
+//that included style anymore. the script tags.
 //
 // I didn't notice any HTML created in the JavaScript-less file.
 // the original file is index.js, vs. index.html
@@ -17,7 +17,13 @@
 // adding script tags.
 //
 // we need the js to connect the class by using getElementsByClassName
-// to get the drop down information. (need to add a class attribute to the list element)
+// to get the drop down information. (need to add a class attribute to the
+// list element)
 //
 // we need an onclickeventhandler or onmouseover event for when the
 // mouse moves over an element (arrow and header)
+//
+// create a function for the accordion.
+
+// use the Document method querySelectorAll to return the list of
+// all of the div elements.
