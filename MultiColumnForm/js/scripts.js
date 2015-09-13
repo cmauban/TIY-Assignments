1ee211f67c753bab5e168e5bdad2decc920bd580
Camille Mauban <camille.mauban@gmail.com>
2015-09-13 16:09:15 -0400

    k

--- MultiColumnForm/js/scripts.js
+++ MultiColumnForm/js/scripts.js
@@ -1,9 +1,15 @@
 
-//1.) Make variables for all of the elements I will be manipulating
+//**********1.) Make variables for all of the elements I will be manipulating
 //    a. start with the form element and one type with in
 //    b. make variable for one of each type of form elements
 //    c. make them all!
 //
+
+// var firstName = document.getElementById('first_name');
+// var lastName = document.getElementById('last_name');
+// var email = document.getElemetById('email');
+
+
 
 //create NodeList for 3 div columns:
 var targetForm = document.getElementsByClassName('.cbp-mc-form');
@@ -14,37 +20,31 @@ console.log(targetDiv);
 var inputIds = document.querySelectorAll('input');
 console.log(inputIds);
 
-function data (){
-  for (var i = 0; i < inputIds.length; i++){
-    inputIds.addEventListener('input', data);
-    console.log(inputIds.value);
-  }
+for (var i = 0; i < inputIds.length; i++){
+  inputIds[i].addEventListener('input', function(){
+    console.log(inputIds[i].value);
+  })
 }
 
 
-// var firstName = document.getElementById('first_name');
-// var lastName = document.getElementById('last_name');
-// var email = document.getElemetById('email');
-//
-// function data (){
-//   console.log(firstName.value);
-// }
-//
-// firstName.addEventListener('input', data);
-// lastName.addEventListener('input', data);
 
 
 
-
-// 2.) Make functions for every(maybe) form element.
+// *******2.) Make functions for every(maybe) form element.
 //    a. make a function that will console.log for the form
 //    b. make a function for one
 //    c. then make function for one of each type of form element
-//    d. then make them all! muwahahaha
+//    d. then make them all!
+
+// function data (){
+//   console.log(firstName.value);
 //
 //preventDefault(); -to prevent the form from being sent. stored in var input
 
 //
-// 3.) Make eventListeners
+// *********3.) Make eventListeners
 //    a. make eventListener for form
 //    b. make eventListeners as needed
+
+// firstName.addEventListener('input', data);
+// lastName.addEventListener('input', data);
