66f51e5d56107b7ae21998a64effed27a5426194
Camille Mauban <camille.mauban@gmail.com>
2015-09-27 15:41:54 -0400

    form logs serialize string

--- MultiColumnForm/js/main.js
+++ MultiColumnForm/js/main.js
@@ -1,16 +1,70 @@
-var inputElements = $('input, select, textarea');
-console.log(inputElements);
 
-// var $submitForm = $('submit');
-$('inputElements').each(function(event) {
-var output = $('inputElements').val();
 
-output.submit();
-  event.preventDefault();
 
-  console.log(output);
-
+//THIS ONE-----
+$('#mcf-submit').submit(function (e) {
+  e.preventDefault();
+  var values = $(this).serialize();
+  console.log(values);
 });
+
+
+
+// $('#mcf-submit').submit(function () {
+
+  // $params = array();
+  // parse_str($_POST, $params);
+  // var values = $(this).serialize();
+//   console.log(values);
+// });
+
+// SERIALIZE.....
+
+// $('#mcf-submit').on('submit', function(e) {
+//
+//   e.preventDefault();
+//   var inputElements = $('#mcf-submit').serialize();
+//   $.post("mcf-submit.php", values, function(data) {
+//     $('#mcf-submit').html(data);
+//
+//
+// });
+// $(function() {
+//
+//   var inputElements = $('input, select, textarea');
+//   console.log(inputElements);
+//
+//   // var $submitForm = $('submit');
+//   $('inputElements').each(function(event) {
+//   var output = $('inputElements').val();
+//
+//   output.submit();
+//     event.preventDefault();
+//
+//     console.log(output);
+//   });
+
+
+
+
+// }); //END FORM FUNCTION
+
+
+
+
+// var inputElements = $('input, select, textarea');
+// console.log(inputElements);
+//
+// // var $submitForm = $('submit');
+// $('inputElements').each(function(event) {
+// var output = $('inputElements').val();
+//
+// output.submit();
+//   event.preventDefault();
+//
+//   console.log(output);
+//
+// });
 
 
 
---
+++ MultiColumnForm/mcf-submit.php
@@ -0,0 +1,10 @@
+<?php
+
+$_POST["firstname"];
+
+$first = $_POST["firstname"];
+$last = $_POST["lastname"];
+
+echo "Hello $first , your last name is $last";
+
+?>
