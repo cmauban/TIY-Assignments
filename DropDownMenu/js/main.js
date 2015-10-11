d21ac445c43dba5d7eb6b17cf45e920f888a63f1
Camille Mauban <camille.mauban@gmail.com>
2015-10-11 17:53:35 -0400

    toggles active class but doesnt show right content

---
+++ DropDownMenu/js/main.js
@@ -0,0 +1,29 @@
+
+$('.tabs').each(function(){
+  $('.tabContent').each(function(){
+
+  $('a').on('click', function(){
+    event.preventDefault(); //stop browser to take action for clicked
+
+    $(this).toggleClass('active');
+    $(this).addClass('active').siblings().removeClass('active');
+    $('.panel').toggleClass('active'); //toggles showing content on and off
+    $('a[#*="panel-"]').toggleClass('panel active');
+    $(this).addClass('active').siblings().removeClass('active');
+
+    $('.panel .active').show().siblings().hide();
+
+
+
+
+
+
+    });
+  });
+});
+
+//
+// $('.tabs').on('click', function(event){
+//     event.preventDefault(); //stop browser to take action for clicked
+//
+//   var active = $(this).attr('href'); //targets active tab
